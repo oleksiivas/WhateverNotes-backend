e8e33c1369bd35b2d55a95fce50930fbaac7c7ab
Alex <vas17010@byui.edu>
2021-02-25 09:14:49 -0700

    views and app

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-// Main js file
+const express = require('express')
+const path = require('path');
+const PORT = process.env.PORT || 5500
 
-// Some text for pull request
-// Hello, World
-// Teaching pull requests
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res, next) => 
+    {
+        res.render('pages/app', {title: 'Whatever Notes', path: '/'});
+    })
+    .listen(PORT, () => console.log('listening on ${ PORT }'))
+
