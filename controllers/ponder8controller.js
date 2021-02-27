03e9fb229413c09118f24cc1d15fd77f511d5f8e
Alex <vas17010@byui.edu>
2021-02-27 13:08:59 -0700

    some pagination code

--- controllers/ponder8controller.js
+++ controllers/ponder8controller.js
@@ -1,5 +1,29 @@
+// const { resolveInclude } = require('ejs');
 const fetch = require('node-fetch');
 const url = "https://byui-cse.github.io/cse341-course/lesson03/items.json";
+
+const items_per_page = 10;
+
+const renderPages = (req, res, json) => {
+    let startingItem = req.body.searchItem || req.query.searchItem || ''
+    let page = req.quety.page || 1
+
+    const indexStart = (page - 1) * items_per_page
+    const indexEnd = page * (items_per_page - 1)
+
+    const filteredData = global.parsedJson.filter(x => x.name.toLowerCase().includes(startingItem.toLowerCase()))
+
+    let result = {
+        data: filteredData.slice(indexStart, indexEnd),
+        path: '/prove08',
+        title: 'Ponder 8',
+        startingItem: startingItem,
+        page: page,
+        numPages: Math.ceil(filteredData.length / items_per_page)
+    }
+
+    res.render('', result)
+}
 
 
 exports.getJson = (req, res, next) => {
