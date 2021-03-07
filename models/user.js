eaecb87973a94d21a7e387f898380d8eea4aa5bb
sarii96 <sara-96325@hotmail.com>
2021-03-06 19:15:03 -0700

    modified models

--- models/user.js
+++ models/user.js
@@ -3,40 +3,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-   
-firstName: {
-    type: String,
-    required: true
-},  
 
-lastName: {
-    type: String,
-    required: true
-},
-   
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
     email: {
         type: String,
         required: true
     },
 
-    password:{
+    password: {
         type: String,
         required: true
-          },
+    },
 
-          note: {
-              notes: [
-                  {
-                      noteId: {
-                          type: Schema.Types.noteId, // I am not sure if I did this right. 
-                          ref: 'Note',
-                          require: true
-                         }
-                 }
-              ]
-     } 
+    notes: [{
+        noteId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Note',
+            require: true
+        }
+    }]
 
-     });
+});
 
 
-    module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
