3b4a0539ed466838d6cb7d0de3f75777012c91c2
Evgeniy Bekker <bek17003@byui.edu>
2021-04-06 12:47:10 -0600

    remove is_auth from chat

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isAuth = require('../middleware/is_auth');
 
-router.get('/', isAuth, (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.render('pages/chat', {
         path: '/chat',
         title: 'Global Chat',
